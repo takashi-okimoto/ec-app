58eefbb7ee98d0577689008d72052357a5709308
takashi-okimoto <okimaru.03@icloud.com>
2020-12-30 19:11:12 +0900

    create Authentication

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCk9pkVb-gH6uLqon1-qGQkfxwdvV3oNwE",
+  authDomain: "ec-app-94b51.firebaseapp.com",
+  projectId: "ec-app-94b51",
+  storageBucket: "ec-app-94b51.appspot.com",
+  messagingSenderId: "504424348853",
+  appId: "1:504424348853:web:b198e913c2be0901b552fc",
+  measurementId: "G-S5GCYKXV80"
+};
---
+++ src/reducks/store/initialState.js
@@ -0,0 +1,12 @@
+const initialState = {
+  products: {
+  },
+  users: {
+    isSignedIn: false,
+    role: "",
+    uid: "",
+    username: ""
+  }
+};
+
+export default initialState
