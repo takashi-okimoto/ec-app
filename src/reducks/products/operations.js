445abc749cbd86d5fbf20959abeeac2115b7e3b2
takashi-okimoto <okimaru.03@icloud.com>
2021-01-03 04:30:01 +0900

    add product list

--- src/reducks/products/operations.js
+++ src/reducks/products/operations.js
@@ -1,7 +1,33 @@
 import { push } from "connected-react-router"
 import { db, FirebaseTimestamp } from "../../firebase"
+import {deleteProductAction,fetchProductsAction} from "./actions";
 
-const productsRef = db.collection('products')
+const productsRef = db.collection('products');
+
+export const deleteProduct = (id) => {
+  return async (dispatch, getState) => {
+    productsRef.doc(id).delete()
+      .then(() => {
+        const prevProducts = getState().products.list;
+        const nextProducts = prevProducts.filter(product => product.id !== id)
+        dispatch(deleteProductAction(nextProducts))
+      })
+  }
+}
+
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    productsRef.orderBy('updated_at', 'desc').get()
+      .then(snapshots => {
+        const ProductList = []
+        snapshots.forEach(snapshot => {
+          const product = snapshot.data();
+          ProductList.push(product)
+        })
+        dispatch(fetchProductsAction(ProductList));
+      })
+  }
+}
 
 export const saveProduct = (id, name, description, price, images) => {
   return async (dispatch) => {
