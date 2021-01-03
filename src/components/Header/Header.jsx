988545c398231e2ef9333db5f43761c5283998e1
takashi-okimoto <okimaru.03@icloud.com>
2021-01-03 19:07:35 +0900

    add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {makeStyles} from "@material-ui/styles";
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import logo from '../../assets/img/icons/logo.jpg';
+import {useDispatch, useSelector } from 'react-redux';
+import { getIsSignedIn } from '../../reducks/users/selectors';
+import {push} from 'connected-react-router';
+import {HeaderMenus} from "./index";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1
+  },
+  menuBar: {
+    backgroundColor: '#fff',
+    color: '#444'
+  },
+  toolBar: {
+    margin: '0 auto',
+    maxWidth: 1024,
+    width: '100%'
+  },
+  iconButtons: {
+    margin: '0 0 0 auto'
+  }
+});
+
+const Header = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const selector = useSelector((state) => state);
+  const isSignedIn = getIsSignedIn(selector)
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.menuBar}>
+        <ToolBar className={classes.toolBar}> 
+          <img
+            src={logo} alt="title logo" width="130px"
+            onClick={() => dispatch(push('/'))}
+          />
+          {isSignedIn && (
+            <div className={classes.iconButtons}>
+              <HeaderMenus />
+            </div>
+          )}
+        </ToolBar>
+      </AppBar>
+    </div>
+  )
+};
+
+export default Header
---
+++ src/components/Header/HeaderMenus.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const HeaderMenus = () => {
+  return (
+    <>
+      <IconButton>
+        <MenuIcon />
+      </IconButton>
+    </>
+  )
+};
+
+export default HeaderMenus
