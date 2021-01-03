63a0002a2a7fee69b463e64e8ef3a125290adf42
takashi-okimoto <okimaru.03@icloud.com>
2021-01-03 21:56:12 +0900

    add drawer menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import {makeStyles} from "@material-ui/styles";
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
@@ -6,7 +6,7 @@ import logo from '../../assets/img/icons/logo.jpg';
 import {useDispatch, useSelector } from 'react-redux';
 import { getIsSignedIn } from '../../reducks/users/selectors';
 import {push} from 'connected-react-router';
-import {HeaderMenus} from "./index";
+import {HeaderMenus, ClosableDrawer} from "./index";
 
 const useStyles = makeStyles({
   root: {
@@ -32,6 +32,15 @@ const Header = () => {
   const selector = useSelector((state) => state);
   const isSignedIn = getIsSignedIn(selector)
 
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerToggle = useCallback((event) => {
+    if (event.type === 'keydown' && (event.key === 'tab' || event.key === 'shift')) {
+      return;
+    }
+    setOpen(!open)
+  }, [setOpen, open]);
+
   return (
     <div className={classes.root}>
       <AppBar position="fixed" className={classes.menuBar}>
@@ -42,11 +51,12 @@ const Header = () => {
           />
           {isSignedIn && (
             <div className={classes.iconButtons}>
-              <HeaderMenus />
+              <HeaderMenus handleDrawerToggle={handleDrawerToggle} />
             </div>
           )}
         </ToolBar>
       </AppBar>
+      <ClosableDrawer open={open} onClose={handleDrawerToggle} />
     </div>
   )
 };
--- src/components/Header/HeaderMenus.jsx
+++ src/components/Header/HeaderMenus.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-const HeaderMenus = () => {
+const HeaderMenus = (props) => {
   return (
     <>
-      <IconButton>
+      <IconButton onClick={(event) => props.handleDrawerToggle(event)}>
         <MenuIcon />
       </IconButton>
     </>
