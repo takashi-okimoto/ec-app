5482f055612355c9bc280633564fb3689a227365
takashi-okimoto <okimaru.03@icloud.com>
2021-01-04 22:42:33 +0900

    add sizeTable

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -3,3 +3,4 @@ export {default as ImagePreview} from './ImagePreview';
 export {default as ImageSwiper} from './ImageSwiper';
 export {default as ProductCard} from './ProductCard';
 export {default as SetSizeArea} from './SetSizeArea';
+export {default as SizeTable} from './SizeTable';
