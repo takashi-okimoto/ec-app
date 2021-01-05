6f113a0635adad6d2dae0ca256fafb70e311d36c
takashi-okimoto <okimaru.03@icloud.com>
2021-01-06 00:51:04 +0900

    add headerIcons

--- src/components/Header/HeaderMenus.jsx
+++ src/components/Header/HeaderMenus.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import MenuIcon from '@material-ui/icons/Menu';
+import {getProductsInCart, getUserId} from "../../reducks/users/selectors";
+import { useDispatch, useSelector } from 'react-redux';
+import { db } from '../../firebase/index';
+import {fetchProductsInCart} from "../../reducks/users/operations";
 
 const HeaderMenus = (props) => {
+  const selector = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const uid = getUserId(selector);
+  let productsInCart = getProductsInCart(selector);
+
+  useEffect(() => {
+    const unsubscribe = db.collection('users').doc(uid).collection('cart')
+      .onSnapshot(snapshots => {
+        snapshots.docChanges().forEach(change => {
+          const product = change.doc.data();
+          const changeType = change.type;
+
+          switch (changeType) {
+            case 'added':
+              productsInCart.push(product);
+              break;
+            case 'modified':
+              const index = productsInCart.findIndex(product => product.cartId === change.doc.id)
+              productsInCart[index] = product;
+              break;
+            case 'removed':
+              productsInCart = productsInCart.filter(product => product.cartId !== change.doc.id)
+              break;
+            default:
+              break;
+          }
+        })
+
+        dispatch(fetchProductsInCart(productsInCart))
+      })
+    return () => unsubscribe()
+  }, []);
+
   return (
     <>
+      <IconButton>
+        <Badge badgeContent={productsInCart.length} color="secondary" >
+        <ShoppingCartIcon />
+        </Badge>
+      </IconButton>
+      <IconButton>
+        <FavoriteBorderIcon />
+      </IconButton>
       <IconButton onClick={(event) => props.handleDrawerToggle(event)}>
         <MenuIcon />
       </IconButton>
