56d15c9d3ea1eddad765b9180e3bd90f88ff0100
takashi-okimoto <okimaru.03@icloud.com>
2021-01-09 19:25:13 +0900

    add shoppingCartPage

--- src/components/Header/HeaderMenus.jsx
+++ src/components/Header/HeaderMenus.jsx
@@ -8,6 +8,7 @@ import {getProductsInCart, getUserId} from "../../reducks/users/selectors";
 import { useDispatch, useSelector } from 'react-redux';
 import { db } from '../../firebase/index';
 import {fetchProductsInCart} from "../../reducks/users/operations";
+import {push} from 'connected-react-router';
 
 const HeaderMenus = (props) => {
   const selector = useSelector((state) => state);
@@ -45,7 +46,7 @@ const HeaderMenus = (props) => {
 
   return (
     <>
-      <IconButton>
+      <IconButton onClick={() => dispatch(push('/cart'))}>
         <Badge badgeContent={productsInCart.length} color="secondary" >
         <ShoppingCartIcon />
         </Badge>
