f969dbd3edff5653ae5e4dd02a08b84787fd6aef
takashi-okimoto <okimaru.03@icloud.com>
2021-01-10 01:16:27 +0900

    add transactionFunc

--- src/templates/index.js
+++ src/templates/index.js
@@ -6,3 +6,4 @@ export {default as Reset} from './Reset';
 export {default as SignIn} from './SignIn';
 export {default as SignUp} from './SignUp';
 export {default as CartList} from './CartList';
+export {default as OrderConfirm} from './OrderConfirm';
